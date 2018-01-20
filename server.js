fe54296c125c90183ea574dcaddf933e81b17013
Shashangka Shekhar <shashangka@gmail.com>
2018-01-20 23:46:50 +0600

    Initial commit

---
+++ server.js
@@ -0,0 +1,20 @@
+'use strict';
+//var http = require('http');
+var express = require('express');
+var app = express();
+var port = process.env.PORT || 1337;
+
+//http.createServer(function (req, res) {
+//    res.writeHead(200, { 'Content-Type': 'text/plain' });
+//    res.end('Hello World\n');
+//}).listen(port);
+
+
+app.get('*', function (req, res) {
+    res.sendFile('index.html', { "root": __dirname });
+});
+
+
+var server = app.listen(port, function () {
+    console.log('Node server is running on port..' + port);
+});
